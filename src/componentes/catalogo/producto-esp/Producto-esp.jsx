c86d6b61f25b420ee2b7e6db481b0af84a5b466f
lilysTlb <lizfer1212@gmail.com>
2026-06-26 17:22:09 -0300

    procucto esp cafes(POR FIN ESTAN LAS IMG)

--- src/componentes/catalogo/producto-esp/Producto-esp.jsx
+++ src/componentes/catalogo/producto-esp/Producto-esp.jsx
@@ -1,10 +1,14 @@
 import "./producto-esp.css";
 import imgcaffe from "../../../assets/imgProductos/CaffeColombia.svg";
+import imgMoneda from "../../../assets/bola.svg"
+import imgMarcoCap from "../../../assets/marcoImg-capsula.svg"
 export default function ProductoEsp(){
     return(
         <div className="producto-esp">
             <div >
                 <div className="producto-esp_div capsula">
+                    <img className="modeda capsula" src={imgMoneda}/>
+                    <img className="marcoImg capsula"src={imgMarcoCap} />
                     <img className="capsula" src={imgcaffe}/>
                 </div>
                 <h3 className="peso capsula">3/4</h3>

145a7d14e76d81ef9cd9f49061ea3f3ac9e362de
lilysTlb <lizfer1212@gmail.com>
2026-06-26 23:35:16 -0300

    añadir mi parte

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from "./componentes/Footer/Footer"
 import PreguntasFrecuentes from "./componentes/PreguntasFrecuentes/PreguntasFrecuentes"
 import Home from './componentes/home/Home';
 import Carrito from './componentes/carrito/Carrito'
+import Catalogo from "./componentes/catalogo/Catalogo"
 import './App.css'
 
 function Inicio() {
@@ -16,6 +17,7 @@ function Inicio() {
       <Link href="/home">Home</Link><br />
       <Link href="/Productos">productos</Link><br />
       <Link href="/carrito">carrito</Link><br />
+      <Link href="/catalogo">carrito</Link><br />
       <Link href="/preguntas">preguntas frecuentes</Link><br />
     </div>
   );
@@ -30,7 +32,7 @@ function App() {
         <Route path="/home" component={Home} />
         <Route path="/carrito" component={Carrito} />
         <Route path="/preguntas" component={PreguntasFrecuentes} />
-        <Route path="/liz" component={Footer} />
+        <Route path="/catalogo" component={Catalogo} />
       </Switch>
     </div>
   );

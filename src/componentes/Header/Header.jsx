c11caa72cd40545d56df56d92d6f6eed88d97b36
Camii-08 <camilam080707@gmail.com>
2026-09-09 14:58:17 -0300

    Cambio de header

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,20 +1,16 @@
 import Style from "./Header.module.css";
 import { Link, useLocation } from "wouter";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import logoHeader from "../../assets/logoHeader.svg";
 import bandera from "../../assets/banner.svg";
 import Carrito from "../modales/Carrito/Carrito";
-export default function Header({onBuscar }) {
+export default function Header({ modo, onBuscar }) {
 
     const [busqueda, setBusqueda] = useState("");
     const [, setLocation] = useLocation();
     const [carritoAbierto, setCarritoAbierto] = useState(false);
     const [cerrandoCarrito, setCerrandoCarrito] = useState(false);
-    const [activo, setActivo] = useState(false);
-    const [activoR, setActivoR] = useState(false);
-    const [pantallaChica, setPantallaChica] = useState(window.innerWidth < 768);
-
     const cerrarCarrito = () => {
         setCerrandoCarrito(true);
 
@@ -23,51 +19,10 @@ export default function Header({onBuscar }) {
             setCerrandoCarrito(false);
         }, 400);
     };
-
-    
-    
-    useEffect(() => {
-
-        const verificarPantalla = () => {
-            const esChica = window.innerWidth < 768;
-
-            setPantallaChica(esChica);
-
-            if (esChica) {
-                setActivo(false);
-                setActivoR(false)
-            }
-        };
-
-        verificarPantalla();
-
-        window.addEventListener("resize", verificarPantalla);
-
-        return () => {
-            window.removeEventListener("resize", verificarPantalla);
-        };
-
-    }, []);
-
-    const volver = () => {
-        console.log("aca estytoy")
-        setActivoR(false)
-    }
-    const search = () => {
-
-        if (pantallaChica == false) {
-            setActivo(true)
-        }
-        else {
-            setActivo(false)
-            setActivoR(true)
-        }
-    }
     const mover = (donde) => {
         window.scrollTo(0, 0)
         setLocation(`/${donde}`);
     }
-
     const buscar = (e) => {
         if (e.key === "Enter") {
             const texto = busqueda.trim();
@@ -81,120 +36,53 @@ export default function Header({onBuscar }) {
             const tipo =
                 new URLSearchParams(window.location.search).get("tipo");
 
-            setLocation(`/catalogo?tipo=${tipo || ""}&busqueda=${encodeURIComponent(texto)}`);
+            setLocation(
+                `/catalogo?tipo=${tipo || ""}&busqueda=${encodeURIComponent(texto)}`
+            );
         }
     };
 
-    
-    if (activoR) {
+    if (modo == "busqueda") {
         return (
-            <SearchMovile
-                mostrar={setActivoR}
-                onBuscar={onBuscar}
-                volver={volver}
-            />
-        );
-    }
-    else {
-        return (
-            <header className={Style.Header}>
+            <header className={Style.Header2}>
 
                 <div className={Style["Header__div"]}>
 
-                    <div className={Style["Header__div-div"]}>
+                    <div
+                        className={`${Style["Header__div-div-nav-iconos"]} ${Style["responsive"]}`}
+                    >
+                        <Link
+                            className={Style["Icono"]}
+                            href="/carrito"
+                        >
+                            <span className="material-symbols-outlined">
+                                arrow_back_ios_new
+                            </span>
+                        </Link>
 
-                        <div className={Style["Header__div-div-logo"]} onClick={() => mover("")}>
-                            <img
-                                src={logoHeader}
-                                alt=""
+                        <div
+                            className={`${Style["Icono"]} ${Style["search"]}`}
+                        >
+
+                            <span className="material-symbols-outlined">
+                                search
+                            </span>
+
+                            <input
+                                type="text"
+                                className={Style.Input}
+                                value={busqueda}
+                                onChange={(e) =>
+                                    setBusqueda(e.target.value)
+                                }
+                                onKeyDown={buscar}
                             />
-                            <h1>
-                                Coffee & Books
-                            </h1>
-                        </div>
-
-
-                        <div className={Style["Header__div-div-nav"]}>
-
-                            <div className={Style["Header__div-div-nav-nav"]}>
-
-                                <h2 className={Style.hache}>
-                                    <Link
-                                        className={Style.Link}
-                                        href="/catalogo"
-                                    >
-                                        Catalogo
-                                    </Link>
-                                </h2>
-
-                                <h2 className={`${Style.hache} ${Style.ulti}`}>
-                                    <a className={Style.Link} href="/#info">
-                                        Sobre mi
-                                    </a>
-                                </h2>
-
-                                <h2 className={Style.hache}>
-                                    <Link
-                                        className={Style.Link}
-                                        href="/"
-                                    >
-                                        Inicio
-                                    </Link>
-                                </h2>
-
-                                <div className={Style["BanderaContainer"]}>
-
-                                    <img
-                                        src={bandera}
-                                        alt=""
-                                        className={Style["Bandera"]}
-                                    />
-
-                                </div>
-
-                            </div>
-
-                            <div className={Style["Header__div-div-nav-iconos"]}>
-                                <div className={`${Style["Icono"]} ${activo ? `${Style["activo"]}` : ""}`} onClick={search}>
-                                    <span className="material-symbols-outlined">
-                                        search
-                                    </span>
-                                    <input
-                                        type="text"
-                                        className={Style.Input}
-                                        value={busqueda}
-                                        onChange={(e) => setBusqueda(e.target.value)}
-                                        onKeyDown={buscar}
-                                    />
-                                </div>
-
-
-                                <Link
-                                    className={Style["Icono"]}
-                                    href="/carrito"
-                                >
-                                    <span className="material-symbols-outlined">
-                                        shopping_cart
-                                    </span>
-                                </Link>
-
-
-                                <div className={Style["Icono"]}>
-                                    <span
-                                        className={
-                                            Style["ic--baseline-whatsapp"]
-                                        }
-                                    ></span>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
-
             </header>
         );
     }
-<<<<<<< HEAD
 
     return (
         <>
@@ -308,6 +196,4 @@ export default function Header({onBuscar }) {
             )}
         </>
     );
-=======
->>>>>>> pulido
 }
